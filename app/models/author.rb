345fec363e24ac748ab72c86c57975b01336c857
Amer Ibric <ameribric@gmail.com>
2022-06-09 19:03:44 -0400

    commit tree d743aa9659f0565dfbb60f44b7fe69d360fcccd7

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    # This could have also said         validates :name, presence: true, uniqueness: true
+    validates :phone_number, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length:  { minimum: 250 }
+    validates :summary, length: { in 0..250 }
+    validates :category, inclusion:  { in: ['Ficton', 'Non-Fiction'] }
+
+
+    validate :you_wont_believe
+    validate :true_facts
+
+    def validate_you_wont_belive
+        unless title == "Won't believe", "Secret", "Top [number]", "Guess"
+            errors.add(:title, "You must put a clickbait title such as: Won't believe, Secret, Top [number], Guess ")
+        end
+    end
+
+    def true_facts
+        if title == "True Facts"
+            errors.add(:title, "You cant put 'True Facts' as your title because we know you are probably not indeed saying 'True Facts'!")
+    end
+  end
+
 end
